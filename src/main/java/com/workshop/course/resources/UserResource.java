2a15592f916ce7f842f762e333b06f57119e5e94
Thiago Brito <thiagobrito74@gmail.com>
2023-11-02 01:12:18 -0400

    H2 database, test profile, JPA

--- src/main/java/com/workshop/course/resources/UserResource.java
+++ src/main/java/com/workshop/course/resources/UserResource.java
@@ -1,7 +1,5 @@
 package com.workshop.course.resources;
 
-import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
