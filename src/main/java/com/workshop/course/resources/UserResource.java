37d6b54ed2a37a37078b542f58d47005fa3129dc
Thiago Brito <thiagobrito74@gmail.com>
2023-11-02 00:51:48 -0400

    User entity and Resource

---
+++ src/main/java/com/workshop/course/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.workshop.course.resources;
+
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.workshop.course.entities.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+
+    @GetMapping
+    public ResponseEntity<User> findAllUsers(){
+        var u = new User(1L, "Maria", "maria@gmail.com", "982327300", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+    
+}
