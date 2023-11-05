7d75f3922bee4538651b506f3554042a7fda4b68
Thiago Brito <thiagobrito74@gmail.com>
2023-11-05 17:17:27 -0400

    User Delete

--- src/main/java/com/workshop/course/resources/UserResource.java
+++ src/main/java/com/workshop/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,11 @@ public class UserResource {
         var obj = userService.createUser(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable(name = "id") Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/workshop/course/services/UserService.java
+++ src/main/java/com/workshop/course/services/UserService.java
@@ -27,5 +27,9 @@ public class UserService {
     public User createUser(User user){
         return userRepository.save(user);
     }
+
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);        
+    }
     
 }
